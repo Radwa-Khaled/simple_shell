660cbecc915fb6ef0a205a65d34c50809ad559cb
ENG: Radwa Khaled <131785821+Radwa-Khaled@users.noreply.github.com>
2023-10-18 17:27:57 +0300

    Update main.c

--- main.c
+++ main.c
@@ -104,44 +104,56 @@ int execute_command(char *command, char **args, char **frnt)
 
 int main(int argc, char *argv[])
 {
-	int ret = 0, retn, *exe_ret = &retn;
+	int ret = 0;
+	int retn;
+	int *exe_ret = &retn;
 	char *prompt = "$ ", *new_line = "\n";
-		char *program_name;
-		int history;
-		alias_t *aliases;
+	char *program_name;
+	int history;
+	alias_t *aliases;
 
-		program_name = argv[0];
-		history = 1;
-		aliases = NULL;
-		signal(SIGINT, signal_handler);
-		*exe_ret = 0;
-		environ = _copyenv();
-		if
-			(!environ) exit(-100);
-		if (argc != 1)
+	program_name = argv[0];
+	history = 1;
+	aliases = NULL;
+	signal(SIGINT, signal_handler);
+
+	*exe_ret = 0;
+	environ = _copyenv();
+	if (!environ)
+		exit(-100);
+
+	if (argc != 1)
+	{
+		ret = proc_file_commands(argv[1], exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	if (!isatty(STDIN_FILENO))
+	{
+		while (ret != END_OF_FILE && ret != EXIT)
+			ret = handle_args(exe_ret);
+		free_env();
+		free_alias_list(aliases);
+		return (*exe_ret);
+	}
+
+	while (1)
+	{
+		write(STDOUT_FILENO, prompt, 2);
+		ret = handle_args(exe_ret);
+		if (ret == END_OF_FILE || ret == EXIT)
 		{
-			ret = proc_file_commands(argv[1], exe_ret);
+			if (ret == END_OF_FILE)
+				write(STDOUT_FILENO, new_line, 1);
+			free_env();
+			free_alias_list(aliases);
+			exit(*exe_ret);
 		}
-		else if (!isatty(STDIN_FILENO))
-		{
-			while
-				((ret != END_OF_FILE) && (ret != EXIT)) ret = handle_args(exe_ret);
-		}
-		else
-		{
-			while (1)
-			{
-				write(STDOUT_FILENO, prompt, 2);
-				ret = handle_args(exe_ret);
-				if
-					((ret == END_OF_FILE) || (ret == EXIT));
-				if
-					(ret == END_OF_FILE) write(STDOUT_FILENO, new_line, 1);
-				exit(*exe_ret);
-			}
-		}
-}
-free_env();
-free_alias_list(aliases);
-return (*exe_ret);
+	}
+
+	free_env();
+	free_alias_list(aliases);
+	return (*exe_ret);
 }
