f37966914aa5d3b3629561e769726df44ba7e534
Radwa-Khaled <radwaabouzed@gmail.com>
2023-10-18 07:22:50 -0700

    fix error

--- main.c
+++ main.c
@@ -102,37 +102,46 @@ int execute_command(char *command, char **args, char **frnt)
  * and handles different modes of input.
  */
 
-int main(int argc, char *argv[]) {
-    int ret = 0, retn, *exe_ret = &retn;
-    char *prompt = "$ ", *new_line = "\n";
-	char *program_name;
-	int history;
-	alias_t *aliases;
-    program_name = argv[0];
-    history = 1;
-    aliases = NULL;
-    signal(SIGINT, signal_handler);
+int main(int argc, char *argv[])
+{
+	int ret = 0, retn, *exe_ret = &retn;
+	char *prompt = "$ ", *new_line = "\n";
+		char *program_name;
+		int history;
+		alias_t *aliases;
 
-    *exe_ret = 0;
-    environ = _copyenv();
-    if (!environ) exit(-100);
-
-    if (argc != 1) {
-        ret = proc_file_commands(argv[1], exe_ret);
-    } else if (!isatty(STDIN_FILENO)) {
-        while ((ret != END_OF_FILE) && (ret != EXIT)) ret = handle_args(exe_ret);
-    } else {
-        while (1) {
-            write(STDOUT_FILENO, prompt, 2);
-            ret = handle_args(exe_ret);
-            if ((ret == END_OF_FILE) || (ret == EXIT)) {
-                if (ret == END_OF_FILE) write(STDOUT_FILENO, new_line, 1);
-                exit(*exe_ret);
-            }
-        }
-    }
-
-    free_env();
-    free_alias_list(aliases);
-    return (*exe_ret);
+		program_name = argv[0];
+		history = 1;
+		aliases = NULL;
+		signal(SIGINT, signal_handler);
+		*exe_ret = 0;
+		environ = _copyenv();
+		if
+			(!environ) exit(-100);
+		if (argc != 1)
+		{
+			ret = proc_file_commands(argv[1], exe_ret);
+		}
+		else if (!isatty(STDIN_FILENO))
+		{
+			while
+				((ret != END_OF_FILE) && (ret != EXIT)) ret = handle_args(exe_ret);
+		}
+		else
+		{
+			while (1)
+			{
+				write(STDOUT_FILENO, prompt, 2);
+				ret = handle_args(exe_ret);
+				if
+					((ret == END_OF_FILE) || (ret == EXIT));
+				if
+					(ret == END_OF_FILE) write(STDOUT_FILENO, new_line, 1);
+				exit(*exe_ret);
+			}
+		}
+}
+free_env();
+free_alias_list(aliases);
+return (*exe_ret);
 }
